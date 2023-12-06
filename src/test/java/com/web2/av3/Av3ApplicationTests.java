87b4615d9c2fcdfc125b416fe2a4554f2ca523c3
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2023-12-05 22:43:05 -0300

    Inicializando projeto

---
+++ src/test/java/com/web2/av3/Av3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.av3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Av3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
